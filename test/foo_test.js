7d81bc48633614c36fddeddd3206818602bb1743
Daniel Perez <tuvistavie@gmail.com>
2014-11-16 19:59:49 +0900

    Fix test.

--- test/foo_test.js
+++ test/foo_test.js
@@ -2,25 +2,6 @@ var content = require('../src/content');
 
 describe("a-test", function() {
   it("should run a test", function() {
-    expect(content).to.eql('kkcontent');
+    expect(content).to.eql('foo.js');
   });
-
-  // it("should require a file", function() {
-  //   var module = require("../fixtures/file.js");
-  //   if(!module.ok) throw new Error("module didn't export ok");
-  // });
-
-  // it("should use loaders", function() {
-  //   var module = require("../fixtures/file.coffee");
-  //   if(!module.ok) throw new Error("module didn't export ok");
-  // });
-
-  // it("should allow chunks", function(done) {
-  //   var test = true;
-  //   require.ensure(["../fixtures/file.js?1"], function(require) {
-  //     test = false;
-  //     done();
-  //   });
-  //   if(!test) throw new Error("Chunk should be async");
-  // });
 });
